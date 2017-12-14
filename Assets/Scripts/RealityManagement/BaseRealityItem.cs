1b3adb4aa307a1aa6554b9c28b1e7ef4ae34a21b
Thiago <trodrigues02@gmail.com>
2017-12-13 23:19:12 -0200

    Created BaseRealityItem class, which simply deactivates renderer and collider from a given object when changing realities

---
+++ Assets/Scripts/RealityManagement/BaseRealityItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseRealityItem : RealityItem
+{
+
+	public Renderer renderer;
+
+	public Collider collider;
+
+
+	private void SetRendererAndCollider (bool status)
+	{
+		renderer.enabled = status;
+		collider.enabled = status;
+	}
+
+	protected override void OnRealitySet (int reality)
+	{
+		if (this.reality == reality)
+			SetRendererAndCollider (true);
+		else
+			SetRendererAndCollider (false);
+	}
+}
